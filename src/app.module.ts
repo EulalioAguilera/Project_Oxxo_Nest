1384afcf76d42ee30c189ffa56b58a5fe491f818
EulalioAguilera <aguilera3ulalio@gmail.com>
2025-10-20 12:34:27 -0600

    new resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,9 @@ import { ConfigModule } from "@nestjs/config";
 import { EmployeesModule } from "./employees/employees.module";
 import { ProductsModule } from "./products/products.module";
 import { ProvidersModule } from './providers/providers.module';
+import { ManagersModule } from './managers/managers.module';
+import { LocationsModule } from './locations/locations.module';
+import { RegionsModule } from './regions/regions.module';
 
 
 @Module({
@@ -22,7 +25,10 @@ import { ProvidersModule } from './providers/providers.module';
     }),
     EmployeesModule,
     ProductsModule,
-    ProvidersModule
+    ProvidersModule,
+    ManagersModule,
+    LocationsModule,
+    RegionsModule
   ],
   controllers: [],
   providers: [],
